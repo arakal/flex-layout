0c46a1fa423eaf65f182ca1602dc5c03f06ce512
Adam Plumer <caerus.karu@gmail.com>
2018-03-26 16:59:56 -0400

    build: add bazel rules

--- src/lib/public-api.ts
+++ src/lib/public-api.ts
@@ -11,6 +11,12 @@
  * @description
  * Entry point for all public APIs of Angular Flex-Layout.
  */
+import {NgModule, Optional, PLATFORM_ID, Inject} from '@angular/core';
+import {isPlatformServer} from '@angular/common';
+import {SERVER_TOKEN} from '@angular/flex-layout/core';
+import {FlexModule} from '@angular/flex-layout/flex';
+import {ExtendedModule} from '@angular/flex-layout/extended';
+
 export * from './version';
 
 export * from '@angular/flex-layout/core';
@@ -18,4 +24,29 @@ export * from '@angular/flex-layout/extended';
 export * from '@angular/flex-layout/flex';
 
 // Flex-Layout Module
-export * from './module';
+/**
+ * Since the equivalent results are easily achieved with a css class attached to each
+ * layout child, these have been deprecated and removed from the API.
+ *
+ *  import {LayoutPaddingDirective} from './api/flexbox/layout-padding';
+ *  import {LayoutMarginDirective} from './api/flexbox/layout-margin';
+ */
+
+/**
+ *
+ */
+@NgModule({
+  imports: [FlexModule, ExtendedModule],
+  exports: [FlexModule, ExtendedModule]
+})
+export class FlexLayoutModule {
+
+  constructor (
+    @Optional() @Inject(SERVER_TOKEN) serverModuleLoaded: boolean,
+    @Inject(PLATFORM_ID) platformId: Object
+  ) {
+    if (isPlatformServer(platformId) && !serverModuleLoaded) {
+      console.warn('Warning: Flex Layout loaded on the server without FlexLayoutServerModule');
+    }
+  }
+}
